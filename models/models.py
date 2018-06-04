f01951856e40813bff065d990fb3bb8d9b86a95b
Valentin BELAUD <valentin@noosys.fr>
2018-06-04 17:35:19 +0200

    basic list product

--- models/models.py
+++ models/models.py
@@ -1,15 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
-# class client_history_product(models.Model):
-#     _name = 'client_history_product.client_history_product'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class client_history_product(models.Model):
+    _inherit = 'res.partner'
+
+    list_products = fields.Many2many(
+        'product.template', compute='_get_last_10_product')
+
+    def _get_last_10_product(self):
+        for partner in self:
+            products = []
+            order_ids = self.env['pos.order'].search([
+                ('partner_id', '=', partner.id)
+            ])
+            for order in order_ids:
+                if order:
+                    for line in order.lines:
+                        if not line.product_id in products:
+                            products.append(line.product_id.product_tmpl_id.id)
+                            if len(products) > 10:
+                                partner.list_products = products
+                                return
+
+            partner.list_products = products
+
+
+
+class PosOrderLine(models.Model):
+    _inherit = 'pos.order.line'
+
+    partner_id = fields.Many2one('pos.order', 'partner_id')
