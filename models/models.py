4ca944b159f4cd52aec068704b275eae873c8b5a
Valentin BELAUD <valentin@noosys.fr>
2018-06-05 15:13:09 +0200

    try to add date order

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, timedelta
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -8,29 +9,37 @@ _logger = logging.getLogger(__name__)
 class client_history_product(models.Model):
     _inherit = 'res.partner'
 
+    # list_products = fields.Many2many(
+    #     'client.product', compute='get_last_10_product')
     list_products = fields.Many2many(
-        'product.template', compute='_get_last_10_product')
+        'product.template', compute='get_last_10_product')
 
-    def _get_last_10_product(self):
+    # Return the last 10 product bought by client on POS.
+    def get_last_10_product(self):
         for partner in self:
             products = []
-            order_ids = self.env['pos.order'].search([
-                ('partner_id', '=', partner.id)
-            ])
+            order_ids = self.env['pos.order'].search(
+                [('partner_id', '=', partner.id)], order='date_order desc')
             for order in order_ids:
                 if order:
+                    # product = self.env['client.product']
+                    # date_order = order.date_order
                     for line in order.lines:
                         if not line.product_id in products:
+                            # products.append(
+                                # product.create(
+                                #     {'product_id': line.product_id.product_tmpl_id.id, 'date_order': date_order})
+                            # )
                             products.append(line.product_id.product_tmpl_id.id)
-                            if len(products) > 10:
+                            if len(products) > 9:
                                 partner.list_products = products
                                 return
 
             partner.list_products = products
 
 
+class ClientProduct(models.Model):
+    _name = 'client.product'
 
-class PosOrderLine(models.Model):
-    _inherit = 'pos.order.line'
-
-    partner_id = fields.Many2one('pos.order', 'partner_id')
+    product_id = fields.Many2one('product.template')
+    date_order = fields.Datetime()
